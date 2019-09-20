0c4fb51bbeaf0fef26963d57bfa1d26b31214d31
Aleksandr Novikov <aleksandr.novikov@spiralscout.com>
2019-09-20 07:28:16 +0300

    [feature] PSR-2 code style fix

--- src/Bootloader/LogStreamBootloader.php
+++ src/Bootloader/LogStreamBootloader.php
@@ -9,7 +9,7 @@
 namespace Spiral\Profiler\Bootloader;
 
 use Spiral\Boot\EnvironmentInterface;
-use Spiral\Core\Bootloader\Bootloader;
+use Spiral\Boot\Bootloader\Bootloader;
 use Spiral\Logger\LogsInterface;
 use Spiral\Profiler\LogStream;
 
